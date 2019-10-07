b277c9293dd26b0d46331d93125a2d30051aa772
Renjie Zhu <renjie.j.zhu@gmail.com>
2019-10-07 15:18:51 -0700

    ->7

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,9 +1,19 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'hw1'))
+	print(os.getcwd())
+except:
+	pass
+    
 #%%
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 import scipy as sp
+import random
 
 data_filename = "amazon_reviews_us_Gift_Card_v1_00.tsv"
 
@@ -55,7 +65,8 @@ plt.bar(rts,nums)
 
 
 #%% [markdown]
-
+# 3.
+# 
 # To train a predictor defined as
 # $ R = \theta_0 + \theta_1 v + \theta_2 l $,
 # we have
@@ -98,6 +109,8 @@ print(f"We have theta_0 = {t_3[0][0]}, theta_1 = {t_3[0][1]}, theta_2 = {t_3[0][
 
 #%% [markdown]
 
+# 4.
+#
 # $\theta_0$ is a value very close to 5. This is obvious from the previous distribution
 # where most reviews are given a five star. $\theta_1$ is a small positive number, and
 # since the possible value is only 0 or 1, this doesn't mean much in this situation.
@@ -130,16 +143,86 @@ print(f"We have theta_0 = {t_4[0][0]}, theta_1 = {t_4[0][1]}.")
 
 #%%
 
-split = int(np.ceil(0.9 * len(data)))
+def split_train_test(data, per):
 
-train_set = data[:split]
-test_set = data[split:]
+    split = int(np.ceil(per * len(data)))
+
+    train_set = data[:split]
+    test_set = data[split:]
+    
+    return train_set, test_set
+
+#%% [markdown]
+
+# 5.
+# 
+# Splitting the data into two portions
 
 #%%
+
+train_set, test_set = split_train_test(data, 0.9)
+
 X_t, R_t = parse_XR(train_set)
 t_train = sp.linalg.lstsq(X_t[:,:2],R_t)
 print(f"For the 90% training set, we have theta_0 = {t_train[0][0]}, theta_1 = {t_train[0][1]}.")
-
+t_train = np.array(t_train[0])
 
 #%%
 
+# using the above calculated \theta on the training data
+
+pred_t = X_t[:,:2] @ t_train.reshape((2,1))
+mse_t = np.mean((R_t.reshape((-1,1)) - pred_t) ** 2)
+
+#%%
+X_test, R_test = parse_XR(test_set)
+pred_test = X_test[:,:2] @ t_train.reshape((2,1))
+mse_test = np.mean((R_test.reshape((-1,1)) - pred_test) ** 2)
+
+#%%
+def train_predict(data, per):
+    train_set, test_set = split_train_test(data, per)
+
+    X_t, R_t = parse_XR(train_set)
+    X_te, R_te = parse_XR(test_set)
+
+    t_train = sp.linalg.lstsq(X_t[:,:2],R_t)
+    t_train = np.array(t_train[0])
+
+    pred_t = X_t[:,:2] @ t_train.reshape((2,1))
+    mse_t = np.mean((R_t.reshape((-1,1)) - pred_t) ** 2)
+
+    pred_te = X_te[:,:2] @ t_train.reshape((2,1))
+    mse_te = np.mean((R_te.reshape((-1,1)) - pred_te) ** 2)
+
+    return mse_t, mse_te
+
+#%% [markdown]
+
+# 7. 
+# 
+# We use different values for the train data
+
+#%%
+train_percentage = np.linspace(0.05, 0.95, 57)
+
+mse_train = []
+mse_test = []
+
+for per in list(train_percentage):
+    mse_t, mse_te = train_predict(data, per)
+    mse_train.append(mse_t)
+    mse_test.append(mse_te)
+
+#%%
+plt.plot(train_percentage, mse_train, label="train")
+plt.plot(train_percentage, mse_test, label="test")
+plt.legend()
+plt.show
+
+
+#%%
+model = linear_model.LogisticRegression()
+model.fit(X_t, R_t)
+
+#%%
